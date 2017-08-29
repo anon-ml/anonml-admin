acd2517e990169ad879d6c83e1bbd762106bf638
Mirco <ablx@posteo.de>
2017-08-29 22:09:54 +0200

    Admin UI for ML

--- src/main/java/ml/anon/admin/ml/MLView.java
+++ src/main/java/ml/anon/admin/ml/MLView.java
@@ -1,21 +1,44 @@
 package ml.anon.admin.ml;
 
+import com.vaadin.server.FileDownloader;
 import com.vaadin.server.FontAwesome;
+import com.vaadin.server.StreamResource;
+import com.vaadin.server.StreamResource.StreamSource;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
+import com.vaadin.ui.Upload;
+import com.vaadin.ui.Upload.Receiver;
+import com.vaadin.ui.Upload.SucceededListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringWriter;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDateTime;
-import javax.annotation.PostConstruct;
+import java.time.format.DateTimeFormatter;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 import ml.anon.admin.BaseView;
+import org.apache.commons.io.output.WriterOutputStream;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
+import org.vaadin.easyuploads.UploadField;
+import org.vaadin.viritin.layouts.MHorizontalLayout;
+import server.droporchoose.UploadComponent;
 
 /**
  * Created by mirco on 16.08.17.
@@ -33,19 +56,18 @@ public class MLView extends BaseView {
   private Label retrainState = new Label();
   private Button retrain = new Button("Training starten", FontAwesome.MAGIC);
   Button download = new Button("Trainingsdaten exportieren", FontAwesome.DOWNLOAD);
-  Button imp = new Button("Trainingsdaten hinzufügen", FontAwesome.UPLOAD);
+  UploadComponent upload;
+  private String url;
 
   public MLView(@Value("${machinelearning.service.url}") String mlUrl) {
     super();
+    url = mlUrl;
+    initUpload();
+    initDownloadButton(download, mlUrl + "/ml/get/training/data/");
     updateRetrainState("Training läuft seit", mlUrl);
-
-    download.addClickListener(
-        e -> log.info(restTemplate
-            .exchange(mlUrl + "/ml/get/training/data/", HttpMethod.GET, null, String.class)
-            .getBody()));
-
     retrain.addClickListener(
         e -> {
+          toggleButtons(false);
           updateRetrainState("Training gestartet ", mlUrl);
           asyncRestTemplate.exchange(mlUrl + "/ml/retrain/", HttpMethod.GET, null, Boolean.class)
               .addCallback(
@@ -67,8 +89,7 @@ public class MLView extends BaseView {
         });
 
     HorizontalLayout layout = new HorizontalLayout();
-
-    layout.addComponents(download, imp, retrain, retrainState);
+    layout.addComponents(download, upload, retrain, retrainState);
     addComponent(layout);
   }
 
@@ -77,15 +98,75 @@ public class MLView extends BaseView {
     String body = restTemplate.getForEntity(url + "/ml/retrain/status", String.class)
         .getBody();
     if (body == null || "".equals(body)) {
-      retrain.setEnabled(true);
-      imp.setEnabled(true);
-      download.setEnabled(true);
+      toggleButtons(true);
       retrainState.setValue("");
     } else {
-      retrain.setEnabled(false);
-      imp.setEnabled(false);
-      download.setEnabled(false);
+      toggleButtons(false);
       retrainState.setValue(msg + " " + body);
     }
   }
+
+  private void initUpload() {
+    upload = new UploadComponent(this::uploadReceived);
+
+    upload.setFailedCallback(this::uploadFailed);
+    upload.setWidth(300, Unit.PIXELS);
+    upload.setHeight(200, Unit.PIXELS);
+    upload.setCaption("Trainingsdaten hinzufügen");
+    upload.setDescription("");
+
+  }
+
+  @SneakyThrows
+  private void uploadReceived(String fileName, Path file) {
+    Notification
+        .show("Trainingsdaten erfolgreich hinzugefügt: " + fileName, Type.TRAY_NOTIFICATION);
+    String str = Files.lines(file).collect(Collectors.joining("\n"));
+    Boolean body = restTemplate
+        .exchange(url + "/ml/post/training/data/", HttpMethod.POST, new HttpEntity<>(str),
+            Boolean.class).getBody();
+    if (body) {
+      Notification
+          .show("Trainingsdaten erfolgreich hinzugefügt: " + fileName, Type.TRAY_NOTIFICATION);
+    } else {
+      Notification
+          .show("Fehler beim Hinzufügen: " + fileName, Type.ERROR_MESSAGE);
+    }
+  }
+
+  private void uploadFailed(String fileName, Path file) {
+    Notification.show("Upload failed: " + fileName, Type.ERROR_MESSAGE);
+  }
+
+  private void toggleButtons(boolean enabled) {
+    retrain.setEnabled(enabled);
+    upload.setEnabled(enabled);
+    download.setEnabled(enabled);
+  }
+
+
+  private void initDownloadButton(Button button, String url) {
+    try {
+      StreamSource streamSource = new StreamSource() {
+
+        @Override
+        public InputStream getStream() {
+          try {
+            return new URL(url).openStream();
+          } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+          }
+        }
+      };
+      FileDownloader fileDownloader = new FileDownloader(
+          new StreamResource(streamSource, "training_data_" + LocalDateTime.now().format(
+              DateTimeFormatter.ISO_DATE_TIME) + ".txt"));
+      fileDownloader.extend(button);
+
+    } catch (Exception e) {
+      log.severe(e.getLocalizedMessage());
+    }
+  }
+
 }
