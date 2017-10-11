434e507322aab1fac827a6f46cd7e0988a215634
mschild <matthiass2006@web.de>
2017-10-11 22:57:51 +0200

    changed server name; build workaround for preloading document

--- src/main/java/ml/anon/ui/common/RedirectController.java
+++ src/main/java/ml/anon/ui/common/RedirectController.java
@@ -1,27 +1,16 @@
 package ml.anon.ui.common;
 
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@RestController("/")
 public class RedirectController {
 
-
-    @Value("${frontend.service.url}")
-    private String frontEndUrl;
-
-    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    @GetMapping
     public RedirectView redirect() {
         return new RedirectView("/overview");
-    }
-
-    @RequestMapping(value = {"/document/{id}"}, method = RequestMethod.GET)
-    public RedirectView adminOverview(@PathVariable("id") String id) {
-        return new RedirectView(frontEndUrl + "/document/" + id);
     }
 }
--- src/main/java/ml/anon/ui/management/DocumentOverviewView.java
+++ src/main/java/ml/anon/ui/management/DocumentOverviewView.java
@@ -61,6 +61,9 @@ public class DocumentOverviewView extends BaseView implements UIEvents.PollListe
     @Value("${documentmanagement.service.url}")
     private String baseUrl;
 
+    @Value("${frontend.service.url}")
+    private String frontEndUrl;
+
     @Resource
     private DocumentResource documentResource;
 
@@ -78,7 +81,7 @@ public class DocumentOverviewView extends BaseView implements UIEvents.PollListe
         grid.addComponentColumn(d -> new MLabel(DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM).format(d.getLastModified()))).setCaption("Last Change");
 
         grid.addComponentColumn((d) -> new MHorizontalLayout(new MButton(FontAwesome.PENCIL, (e) -> {
-            getUI().getPage().open( "/document/" + d.getId(), "", false);
+            getUI().getPage().open(frontEndUrl + "/document/" + d.getId(), "", false);
         }).withStyleName(ValoTheme.BUTTON_BORDERLESS), initDownloadButton(d.fileNameAs("zip"), baseUrl + "/document/" + d.getId() + "/export", FontAwesome.DOWNLOAD),
                 initDownloadButton(d.getFileName(), baseUrl + "/document/" + d.getId() + "/original", FontAwesome.FILE_TEXT_O), new MButton(FontAwesome.TRASH, (e) -> {
             ConfirmDialog.show(UI.getCurrent(), (ee) -> {
