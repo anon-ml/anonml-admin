4c6549db4ef1ffdda8bca417cd5c3107a8523ea4
mschild <matthiass2006@web.de>
2017-10-11 23:10:13 +0200

    fixed the redirect on admin overview

--- src/main/java/ml/anon/ui/common/RedirectController.java
+++ src/main/java/ml/anon/ui/common/RedirectController.java
@@ -1,6 +1,7 @@
 package ml.anon.ui.common;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,11 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestController("/")
 public class RedirectController {
 
+    @Value("${server.contextPath}")
+    private String serverContextPath;
+
     @GetMapping
     public RedirectView redirect() {
-        return new RedirectView("/overview");
+        return new RedirectView(serverContextPath + "/overview");
     }
 }
